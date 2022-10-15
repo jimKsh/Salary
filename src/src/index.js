f4b1998e3cefaf07f676d40d11c09f8815821fe0
jimmy <jim.ksh@gmail.com>
2022-10-15 22:48:13 +0300

    update dependencies

--- src/src/index.js
+++ src/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { WorkProvider } from './context/Work';
 import './index.css';
 import App from './pages/App/App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,8 +15,3 @@ root.render(
     </WorkProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
